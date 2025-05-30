7f97b406faccfe37e0e802eba7c335de82c2d880
normalplayar <chrissh37@gmail.com>
2025-05-30 16:22:07 +0700

    Revert "Updated UI, removed old components, added new pages"
    This reverts commit 21ccad7b380729bfe4bce9ad1b03d26a94801716.

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,15 @@
-// src/App.js
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Shop from './pages/shop';
-import Cart from './pages/Cart';
-import Account from './pages/Account/Account';
-import Login from './pages/Account/Login';
-import Register from './pages/Account/Register';
-import Settings from './pages/Account/Settings';
-import Ticket from './pages/Customer/Ticket';
-import TicketManagement from './pages/Staff/TicketManagement';
-import StaffManagement from './pages/Admin/StaffManagement';
-import PrivateRoute from './components/PrivateRoute';
-import { CartProvider } from './Context/CartContext';
-import Navbar from './components/navbar';
+import { useState } from 'react';
+import './App.css';
+import NavBar from './components/NavBar/navbar';
 
 function App() {
+  
+
   return (
-    <CartProvider>
-      <Navbar />
-      <Routes>
-        {/* Public Routes */}
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/cart" element={<Cart />} />
-        
-        {/* Account Routes */}
-        <Route path="/account">
-          <Route index element={<Account />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-        </Route>
-        
-        {/* Protected Routes */}
-        <Route element={<PrivateRoute allowedRoles={['customer', 'staff', 'admin']} />}>
-          <Route path="/account/settings" element={<Settings />} />
-        </Route>
-        
-        {/* Customer-only Routes */}
-        <Route element={<PrivateRoute allowedRoles={['customer']} />}>
-          <Route path="/customer/ticket" element={<Ticket />} />
-        </Route>
-        
-        {/* Staff-only Routes */}
-        <Route element={<PrivateRoute allowedRoles={['staff', 'admin']} />}>
-          <Route path="/staff/tickets" element={<TicketManagement />} />
-        </Route>
-        
-        {/* Admin-only Routes */}
-        <Route element={<PrivateRoute allowedRoles={['admin']} />}>
-          <Route path="/admin/staff" element={<StaffManagement />} />
-        </Route>
-      </Routes>
-    </CartProvider>
-  );
+    <>
+      <NavBar />
+    </>
+  )
 }
 
-export default App;
+export default App
