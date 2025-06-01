50fa826e2058a78b6aab33de83654611616c2fed
normalplayar <chrissh37@gmail.com>
2025-06-02 00:43:29 +0700

    Update About.jsx

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './About.css';
 
-import Tambora from '../img/tambora-branch.png';
-import Cengkareng from '../img/cengkareng-branch.png';
+import Tambora from '../img/Tambora-branch.png';
+import Cengkareng from '../img/Cengkareng-branch.png';
 import WA from '../img/WA-logo.png';
 import Phone from '../img/phone-icon.png';
 import Calendar from '../img/calendar-icon.png';
