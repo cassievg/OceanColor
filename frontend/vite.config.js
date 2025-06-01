a7a6da2e4c59dcc62ddbfc490238dc62a21573f2
cassievg <cassievalleriagarcia@gmail.com>
2025-06-01 21:48:38 +0700

    deploy cicd pipeline

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    server: {
+      proxy: {
+        "/api": {
+          target: mode === "development"
+            ? "http://localhost:5000"
+            : "https://e2425-wads-l4acg4-server.csbihub.id",
+          changeOrigin: true,
+          secure: mode !== "development",
+          ws: true,
+        },
       },
-    }
+    },
+    build: {
+      outDir: "dist",
+    },
+    plugins: [react(), tailwindcss()],
   }
 })
